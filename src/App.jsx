d99fa1307cfdf8029a440312d914eeaec7ca54bd
supremeoverlord-ofdoom <108652766+supremeoverlord-ofdoom@users.noreply.github.com>
2023-02-11 12:25:36 +0800

    removing hardcoded values

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import TodoItem from "./components/TodoItem";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Learn about React",
+    "Meet friend for lunch",
+    "Build really cool todo app",
+  ]);
+  
   return (
       <div>
       <h1>My todo list</h1>
-      <TodoItem todo="Finish plus project." />
-      <TodoItem todo="Feed cat." />
-      <TodoItem todo="Be awesome." />
+      {todos.map((todo, index) => (
+      <TodoItem todo={todo} key={index} />
+      ))}
       </div>
     );
   }
