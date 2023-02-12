c87a1e025dc6febce5405fed0ad9c40a1bca1c94
supremeoverlord-ofdoom <108652766+supremeoverlord-ofdoom@users.noreply.github.com>
2023-02-12 22:41:17 +0800

    style to do item
    step 9 completed

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./TodoItem.css";
 
 function TodoItem(props) {
     const { todo } = props;
-    return <div>{todo}</div>;
+    return <div className="todo">{todo}</div>;
 }
 
 export default TodoItem;
